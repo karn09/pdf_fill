1bfec840417bd0be27d7846121fe63c18ff99c2f
John Nieves <karn09@gmail.com>
2015-08-26 23:24:20 -0400

    cleaned up code

--- convertCSV2.js
+++ convertCSV2.js
@@ -2,42 +2,48 @@ var csv = require('fast-csv');
 var fdf = require('fdf.js');
 var fs = require('fs');
 var childPrc = require('child_process');
-var stream = fs.createReadStream("8.28.csv");
-var counter = 0;
 
+var CSVINPUT = "2015perf.csv"
+var PDFTEMPLATE = '2015_perf_temp.pdf';
+// command line arguments:
+// CSV input
+// headers array
+// template name
 
-var csvData = [];
+var stream = fs.createReadStream(CSVINPUT);
 
 var createFDFfromCSV = csv.fromStream(stream, {
-  headers: ["Name", "Position", "Date", "Reviewer"]
+  headers: ["Name", "Date", "Position", "Reviewer"]
 });
 
 
 createFDFfromCSV.on("record", function(data) {
-  counter++;
   var newFdf = fdf.generate(data);
-  var fileName = 'record' + counter + '.fdf';
-  var pdfFileName = __dirname + '/filled/' + (data['Reviewer'] + '-' + data['Name']).split(' ').join('') + '.pdf';
-  //  console.log(pdfFileName);
+  var fileName = data['Name'] + '-' + data['Reviewer'] + '.fdf';
+  //var pdfFileName = __dirname + '/filled/' + (data['Reviewer'] + '-' + data['Name']).split(' ').join('') + '.pdf';
   var tempFdfFile = __dirname + '/temp/' + fileName;
   fs.writeFile(tempFdfFile, newFdf);
-  //  generatePDF(pdfFileName, tempFdfFile); // attempt to call spawn processes as templates creaed
-  // not working, gives some unknown error and stops
+  fillPdf(data)
 }).on("end", function(numRecords) {
-  counter = 0;
   console.log(numRecords + " forms created.");
 });
 
+function fillPdf (data) {
 
-// var generatePDF = function(pdfFileName, tempFdfFile) {
-//   //  var filledPdf = __dirname + '/filled/' + 'filledRec' + i + '.pdf';
-//   //  var fileName = 'record' + i + '.fdf';
-//   //  var tempFdfFile = __dirname + '/temp/' + fileName;
-//   childPrc.spawn('pdftk', ['template.pdf', 'input_pw', 'eval2014', 'fill_form', tempFdfFile, 'output', pdfFileName, 'user_pw', 'eval2014', 'allow', 'AllFeatures']);
-// };
-for (var i = 1; i <= 309; i++) {
-  var pdfFileName = __dirname + '/filled/' + 'filledRec' + i + '.pdf';
-  var fileName = 'record' + i + '.fdf';
-  var tempFdfFile = __dirname + '/temp/' + fileName;
-  childPrc.spawn('pdftk', ['template.pdf', 'input_pw', 'eval2014', 'fill_form', tempFdfFile, 'output', pdfFileName, 'user_pw', 'eval2014', 'allow', 'AllFeatures']);
+  var pdfFileName = __dirname + '/filled/' + data['Name'] + '-' + data['Reviewer'] + '.pdf';
+  var tempFdfFile = __dirname + '/temp/' + data['Name'] + '-' + data['Reviewer'] + '.fdf';
+
+//var proc = 'pdftk ' + pdfTemplate  + ' input_pw eval2015 fill_form ' + tempFdfFile + ' output ' + pdfFileName + ' user_pw eval2015 allow AllFeatures ';
+//  childPrc.exec(proc,
+//   function (error, stdout, stderr) {
+//     console.log('stdout: ' + stdout);
+//     console.log('stderr: ' + stderr);
+//     if (error !== null) {
+//       console.log('exec error: ' + error);
+//     }
+// });
+  var writePdf = childPrc.spawn('pdftk', [PDFTEMPLATE, 'input_pw', 'eval2015', 'fill_form', tempFdfFile, 'output', pdfFileName, 'user_pw', 'eval2015', 'allow', 'AllFeatures']);
+  writePdf.on('exit', function (exitCode) {
+    console.log(pdfFileName + ' written to disk.');
+  })
 }
