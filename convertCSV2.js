66379cc010049ea9b6592254b999a25db66abc87
John Nieves <karn09@gmail.com>
2018-08-02 13:05:52 -0400

    updated converter

--- convertCSV2.js
+++ convertCSV2.js
@@ -1,94 +1,64 @@
-const csv = require("fast-csv");
-const fdf = require("fdf.js");
-const generator = require("utf8-fdf-generator").generator;
-const fdfRoot = "temp";
-const pdfRoot = "filled";
-var qpdf = require("node-qpdf");
-const _ = require("lodash");
-var childPrc = require("child_process");
-var CSVINPUT = process.argv[2];
-const PDFTEMPLATE = "perf.pdf"; // set me
-csv
-  .fromPath(CSVINPUT, {
+const csv = require('fast-csv');
+const generator = require('utf8-fdf-generator').generator;
+const fdfRoot = 'temp';
+const pdfRoot = 'filled';
+const _ = require('lodash');
+// var childPrc = require("child_process");
+const execa = require('execa');
+const CSVINPUT = process.argv[2];
+const PDF_FILE = process.argv[3];
+const PASSWORD = process.argv[4];
+const PDFTEMPLATE = PDF_FILE;
+
+csv.fromPath(CSVINPUT, {
     objectMode: true,
     headers: true,
     quote: null,
     strictColumnHandling: false,
     discardUnmappedColumns: true
-  })
-  .on("data", function(data) {
-    const reviewers = _.values(
-      _.pickBy(data, (value, key) => {
-        if (key && _.startsWith(key, "reviewed") && value) {
-          return key;
+})
+    .on('data', async function(data) {
+        try {
+            const fdfName = `${data.Username}_for_${
+                data.Name
+            }_PerformanceReview2018.fdf`;
+            // Username_for_Name_PerformanceReview2018
+            const pdfName = `${data.Username}_for_${
+                data.Name
+            }_PerformanceReview2018.pdf`;
+            await generator(
+                {
+                    Name: _.get(data, 'Name', '').trim(),
+                    Position: _.get(data, 'Position', '').trim(),
+                    Date: _.get(data, 'Date', '').trim(),
+                    Reviewer: _.get(data, 'Reviewer', '').trim()
+                },
+                `${fdfRoot}/${fdfName}`
+            );
+            await fillPdf(pdfName, fdfName);
+            console.log(pdfName);
+        } catch (err) {
+            console.log(err);
         }
-      })
-    );
-    const reviewee = _.pickBy(data, (value, key) => {
-      if (key && !_.startsWith(key, "reviewed") && value) {
-        return key;
-      }
-    });
-    const pdfs = reviewers.map(reviewer => {
-      return {
-        ...reviewee,
-        reviewer
-      };
+    })
+    .on('end', function(numRecords) {
+        console.log(numRecords + ' forms created.');
     });
 
-    pdfs.forEach(async pdf => {
-      try {
-        const fdfName = `${pdf.name}-${pdf.reviewer}.fdf`;
-        const pdfName = `${pdf.reviewer}_for_${pdf.name}_2017.pdf`;
-        await generator(
-          {
-            Name: _.get(pdf, "name", "").trim(),
-            Position: _.get(pdf, "position", "").trim(),
-            Date: _.get(pdf, "date", "").trim(),
-            Reviewer: _.get(pdf, "reviewer", "").trim()
-          },
-          `${fdfRoot}/${fdfName}`
-        );
-        await fillPdf(pdfName, fdfName);
-        console.log(pdfName);
-      } catch (err) {
-        console.log(err);
-      }
-    });
-  })
-  .on("end", function(numRecords) {
-    console.log(numRecords + " forms created.");
-  })
-
-function qp(localFilePath) {
-  var options = {
-    keyLength: 128,
-    password: "YOUR_PASSWORD_TO_ENCRYPT",
-    print: "full",
-    modify: "all",
-    // restrictions: {
-    // }
-  };
-
-  qpdf.encrypt(localFilePath, options);
-}
 function fillPdf(pdfPath, fdfPath) {
-  var pdfFileName = `${pdfRoot}/${pdfPath}`;
-  var tempFdfFile = `${fdfRoot}/${fdfPath}`;
-  var writePdf = childPrc.spawnSync("pdftk", [
-    PDFTEMPLATE,
-    "fill_form",
-    tempFdfFile,
-    "output",
-    pdfFileName,
-    // "user_pw",
-    // "2017​PerfRev",
-    "allow",
-    "AllFeatures",
-    // "verbose"
-  ]);
-  // writePdf.on("exit", function(exitCode) {
-  //   console.log(pdfFileName + " written to disk.");
-  // });
-  // writePdf.on("error", console.error);
+    var pdfFileName = `${pdfRoot}/${pdfPath}`;
+    var tempFdfFile = `${fdfRoot}/${fdfPath}`;
+
+    return execa('pdftk', [
+        PDFTEMPLATE,
+        'fill_form',
+        tempFdfFile,
+        'output',
+        pdfFileName,
+        "user_pw",
+        PASSWORD,
+        'allow',
+        'AllFeatures'
+        // "verbose"
+    ]);
 }
