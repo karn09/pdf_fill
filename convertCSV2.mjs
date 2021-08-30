eb7c739a822e54ea1888dab2a5d4a32403062b02
karn09 <jnieves@justworks.com>
2021-08-29 22:22:41 -0400

    rename and remove unneeded code

--- convertCSV2.mjs
+++ convertCSV2.mjs
@@ -1,23 +1,9 @@
 import { parseStream } from "fast-csv";
 import * as fs from "fs";
-// const generator = require('utf8-fdf-generator').generator;
-const fdfRoot = "temp";
-const pdfRoot = "filled";
-// const _ = require('lodash');
-// var childPrc = require("child_process");
-// const execa = require('execa');
-const CSVINPUT = process.argv[2];
-const PDF_FILE = process.argv[3];
-const PASSWORD = process.argv[4];
-const PDFTEMPLATE = PDF_FILE;
 
-class CSV {
-  static fdfTemplatePath = "/tmp/convert_csv/fdf";
-
-  constructor(pathToCSV, pathToInputPDF) {
+class CSVReader {
+  constructor(pathToCSV) {
     this.csvStream = fs.createReadStream(pathToCSV);
-    this.pathToInputPDF = pathToInputPDF;
-    // this.outputFormat = outputFormat;
   }
 
   async getHeaders() {
@@ -66,60 +52,4 @@ class CSV {
   }
 }
 
-class FdfFactory {}
-let a = new CSV("./2021PR.csv");
-console.log(await a.getHeaders());
-
-// return;
-// csv.fromPath(CSVINPUT, {
-//     objectMode: true,
-//     headers: true,
-//     quote: null,
-//     strictColumnHandling: false,
-//     discardUnmappedColumns: true
-// })
-//     .on('data', async function(data) {
-//         try {
-//             const fdfName = `${data.Username}_for_${
-//                 data.Name
-//             }_PerformanceReview2018.fdf`;
-//             // Username_for_Name_PerformanceReview2018
-//             const pdfName = `${data.Username}_for_${
-//                 data.Name
-//             }_PerformanceReview2018.pdf`;
-//             await generator(
-//                 {
-//                     Name: _.get(data, 'Name', '').trim(),
-//                     Position: _.get(data, 'Position', '').trim(),
-//                     Date: _.get(data, 'Date', '').trim(),
-//                     Reviewer: _.get(data, 'Reviewer', '').trim()
-//                 },
-//                 `${fdfRoot}/${fdfName}`
-//             );
-//             await fillPdf(pdfName, fdfName);
-//             console.log(pdfName);
-//         } catch (err) {
-//             console.log(err);
-//         }
-//     })
-//     .on('end', function(numRecords) {
-//         console.log(numRecords + ' forms created.');
-//     });
-
-// function fillPdf(pdfPath, fdfPath) {
-//     var pdfFileName = `${pdfRoot}/${pdfPath}`;
-//     var tempFdfFile = `${fdfRoot}/${fdfPath}`;
-
-//     return execa('pdftk', [
-//         PDFTEMPLATE,
-//         'fill_form',
-//         tempFdfFile,
-//         'output',
-//         pdfFileName,
-//         "user_pw",
-//         PASSWORD,
-//         'allow',
-//         'AllFeatures'
-//         // "verbose"
-//     ]);
-// }
+export default CSVReader;
